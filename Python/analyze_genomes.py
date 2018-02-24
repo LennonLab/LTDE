6712f1785f0d66a9c1337f94d5bcf330600718bb
Will Shoemaker <shoemakah@gmail.com>
2018-02-23 23:32:37 -0500

    pPCA, pathways, raxml, align

--- Python/analyze_genomes.py
+++ Python/analyze_genomes.py
@@ -1,18 +1,19 @@
 from __future__ import division
-import os, re
+import os, re, glob
 import pandas as pd
 import numpy as np
+from Bio import SeqIO
 
 mydir = os.path.expanduser("~/GitHub/LTDE/")
 
 
 strains = ['ATCC13985', 'ATCC43928', 'KBS0701', 'KBS0702', 'KBS0703', 'KBS0705', \
-            'KBS0706', 'KBS0707', 'KBS0710', 'KBS0711', 'KBS0712', 'KBS0714', \
-            'KBS0715', 'KBS0721', 'KBS0722', 'KBS0724', 'KBS0725', 'KBS0801', \
+            'KBS0706', 'KBS0707', 'KBS0710', 'KBS0711', 'KBS0712', 'KBS0713', 'KBS0714', \
+            'KBS0715', 'KBS0721', 'KBS0722', 'KBS0724', 'KBS0725', 'KBS0727', 'KBS0801', \
             'KBS0802', 'KBS0812']
 
 def module_to_KO(strain):
-    kaas_directory = mydir + 'data/MAPLE/' + strain + '_MAPLE_result/KAAS'
+    kaas_directory = mydir + 'data/metab_paths/MAPLE/' + strain + '_MAPLE_result/KAAS'
     data = [['KEGG_Orthology', 'Pathway_ID']]
     bad_chars = '()-+,-'
     rgx = re.compile('[%s]' % bad_chars)
@@ -40,7 +41,7 @@ def module_to_KO(strain):
                                 data.append([ko_gene, pathway])
 
     df = pd.DataFrame(data[1:],columns=data[0])
-    OUT_path =  mydir + 'data/MAPLE_clean/' + strain + '_MAPLE_clean/'
+    OUT_path =  mydir + 'data/metab_paths/MAPLE_clean/' + strain + '_MAPLE_clean/'
     if not os.path.exists(OUT_path):
         os.makedirs(OUT_path)
     OUT_file_path = OUT_path + 'KO_to_M.txt'
@@ -48,11 +49,11 @@ def module_to_KO(strain):
 
 
 def clean_kaas(strain):
-    IN_kaas_path = mydir + 'data/KAAS/' + strain + '_KAAS_result_ko'
+    IN_kaas_path = mydir + 'data/metab_paths/KAAS/' + strain + '_KAAS_result_ko'
     IN_kaas = pd.read_csv(IN_kaas_path, sep = '\t',
         names = ['protein_id', 'KO', 'species', 'phylum_genus', 'num'])
     IN_kaas_subset = IN_kaas.loc[IN_kaas['KO'] != 'K_NA']
-    OUT_path = mydir + 'data/KAAS_clean/' + strain + '_KAAS_clean.txt'
+    OUT_path = mydir + 'data/metab_paths/KAAS_clean/' + strain + '_KAAS_clean.txt'
     OUT = open(OUT_path, 'w')
     print>> OUT, 'protein_id', 'KEGG_Orthology', 'species', 'phylum', 'genus', 'num'
     count = 0
@@ -80,13 +81,13 @@ def clean_kaas(strain):
 
 
 def merge_maple(strain):
-    IN_maple_sign_path = mydir + 'data/MAPLE/' + strain + '_MAPLE_result/' + 'module_signature.tsv'
+    IN_maple_sign_path = mydir + 'data/metab_paths/MAPLE/' + strain + '_MAPLE_result/' + 'module_signature.tsv'
     IN_maple_sign = pd.read_csv(IN_maple_sign_path, sep = '\t')
-    IN_maple_cmplx_path = mydir + 'data/MAPLE/' + strain + '_MAPLE_result/' + 'module_complex.tsv'
+    IN_maple_cmplx_path = mydir + 'data/metab_paths/MAPLE/' + strain + '_MAPLE_result/' + 'module_complex.tsv'
     IN_maple_cmplx = pd.read_csv(IN_maple_cmplx_path, sep = '\t')
-    IN_maple_pthwy_path = mydir + 'data/MAPLE/' + strain + '_MAPLE_result/' + 'module_pathway.tsv'
+    IN_maple_pthwy_path = mydir + 'data/metab_paths/MAPLE/' + strain + '_MAPLE_result/' + 'module_pathway.tsv'
     IN_maple_pthwy = pd.read_csv(IN_maple_pthwy_path, sep = '\t')
-    IN_maple_fxn_path = mydir + 'data/MAPLE/' + strain + '_MAPLE_result/' + 'module_function.tsv'
+    IN_maple_fxn_path = mydir + 'data/metab_paths/MAPLE/' + strain + '_MAPLE_result/' + 'module_function.tsv'
     IN_maple_fxn = pd.read_csv(IN_maple_fxn_path, sep = '\t')
     df_list = [IN_maple_cmplx, IN_maple_pthwy, IN_maple_sign]
     df_merged = IN_maple_fxn.append(df_list)
@@ -95,13 +96,13 @@ def merge_maple(strain):
     df_merged_no_dup = df_merged.drop_duplicates(subset='Pathway_ID', keep="last")
     df_merged_no_dup = df_merged_no_dup.reset_index(drop=True)
     # median = median MCR
-    OUT_path = mydir + 'data/MAPLE_clean/' + strain + '_MAPLE_clean/maple_modules.txt'
+    OUT_path = mydir + 'data/metab_paths/MAPLE_clean/' + strain + '_MAPLE_clean/maple_modules.txt'
     df_merged_no_dup.to_csv(OUT_path, sep = '\t', index = False)
 
 
 def merge_maple_all_strains():
     dfs = []
-    maple_path = mydir + 'data/MAPLE_clean/'
+    maple_path = mydir + 'data/metab_paths/MAPLE_clean/'
     for subdir, dirs, files in os.walk(maple_path):
         for f in files:
             if f == 'maple_modules.txt':
@@ -119,7 +120,7 @@ def merge_maple_all_strains():
     dfs_concat_050 = dfs_concat.loc[dfs_concat['query(coverage)'] >= 0.5]
     module_by_taxon = pd.crosstab(dfs_concat_050.Pathway_ID, dfs_concat_050.Strain)
     #module_by_taxon_no_redundant = module_by_taxon[(module_by_taxon.T != 1).any()]
-    OUT_path = mydir + 'data/module_by_taxon.txt'
+    OUT_path = mydir + 'data/metab_paths/module_by_taxon.txt'
     module_by_taxon.to_csv(OUT_path, sep = '\t', index = True)
 
 
@@ -129,5 +130,14 @@ def run_all_strains():
         clean_kaas(strain)
         merge_maple(strain)
 
-#run_all_strains()
+
+
+
+run_all_strains()
 merge_maple_all_strains()
+# yes
+# L2
+# no
+# L3
+
+#get_16SrRNA_seqs()
--- bin/persistence_707_712_721.sh
+++ bin/persistence_707_712_721.sh
@@ -5,12 +5,20 @@
 #PBS -m abe
 #PBS -j oe
 
-module load gcc/4.9.2
-module load boost/1.52.0
-module load openmpi
-module load mothur/1.38.1
+#module load gcc/4.9.2
+#module load boost/1.52.0
+#module load openmpi
+#module load mothur/1.38.1
+module load mothur
 
 
 cd /N/dc2/projects/Lennon_Sequences/LTDE_Tree
+
+#wget https://www.mothur.org/w/images/d/dc/Trainset16_022016.rdp.tgz
+#wget https://www.mothur.org/w/images/7/71/Silva.seed_v132.tgz
+#tar -xzvf Silva.seed_v132.tgz
+#mv Trainset16_022016.rdp.tgz silva.v4.fasta
+
+
 
 mothur persistence_707_712_721.batch
