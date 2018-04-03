48a35b1425020e5298a25cdc01b2db42cfff926e
Will Shoemaker <shoemakah@gmail.com>
2018-04-03 16:14:49 -0400

    new outgroup, fixed survivorship figs

--- bin/raxml.sh
+++ bin/raxml.sh
@@ -1,26 +1,25 @@
 #!/bin/bash
 #PBS -k o
-#PBS -l nodes=2:ppn=8,vmem=100gb,walltime=5:00:00
+#PBS -l nodes=2:ppn=8,vmem=100gb,walltime=240:00:00
 #PBS -M wrshoema@indiana.edu
 #PBS -m abe
 #PBS -j oe
 
-module load raxml/8.0.26
-module load raxml/8.2.11
-
+# version 8.2.11
+#module load raxml
 
 # cd into the directory with your alignment
 
-cd /N/dc2/projects/Lennon_Sequences/LTDE_Tree/tree
+#cd /N/dc2/projects/Lennon_Sequences/LTDE_Tree/tree
 
 #raxmlHPC-PTHREADS -T 4 -f a -m GTRGAMMA -p 12345 -x 12345 -o Methanosarcina -# autoMRE \
 #    -s /N/dc2/projects/Lennon_Sequences/LTDE_Tree/tree/persistence_707_712_721.good.filter.fasta \
 #    -n T20 -w /N/dc2/projects/Lennon_Sequences/LTDE_Tree/tree/
 
 
-raxmlHPC-PTHREADS -T 4 -f a -m GTRGAMMA -p 12345 -x 12345 -o Methanosarcina -# autoMRE \
-    -s /Users/WRShoemaker/GitHub/LTDE/phylo/tree/persistence_707_712_721.good.filter.fasta \
-    -n T20 -w /Users/WRShoemaker/GitHub/LTDE/phylo/tree
+raxmlHPC-PTHREADS -T 4 -f a -m GTRGAMMA -p 12345 -x 12345 -o NC_005042.1_353331-354795 -# autoMRE \
+    -s /Users/WRShoemaker/GitHub/LTDE/data/align/persistence_prochlorococcus_genomes.good.filter.fasta \
+    -n OutProGen -w /Users/WRShoemaker/GitHub/LTDE/data/tree
 
 # -T = number of threads
 # -f = specifies bootstrapping algorithm with ML generating tree at same time
