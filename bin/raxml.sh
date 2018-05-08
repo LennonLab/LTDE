412ca038cf678cad882c861f3515f5c505e7edc3
Will Shoemaker <shoemakah@gmail.com>
2018-05-07 23:06:21 -0400

    add fasttree

--- bin/raxml.sh
+++ bin/raxml.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 #PBS -k o
-#PBS -l nodes=2:ppn=8,vmem=100gb,walltime=240:00:00
+#PBS -l nodes=1:ppn=8,vmem=100gb,walltime=1000:00:00
 #PBS -M wrshoema@indiana.edu
 #PBS -m abe
 #PBS -j oe
 
 # version 8.2.11
-#module load raxml
+module load raxml
 
-raxmlHPC-PTHREADS -T 4 -f a -m GTRGAMMA -p 12345 -x 12345 -o NC_005042.1_353331-354795 -# autoMRE \
-    -s /Users/WRShoemaker/GitHub/LTDE/data/align/ltde_seqs.good.filter.fasta \
-    -n ltde_seqs -w /Users/WRShoemaker/GitHub/LTDE/data/tree
+#raxmlHPC-PTHREADS -T 4 -f a -m GTRGAMMA -p 12345 -x 12345 -o NC_005042.1_353331-354795 -# autoMRE \
+#    -s /Users/WRShoemaker/GitHub/LTDE/data/align/16S_rRNA_seqs/ltde_seqs.good.filter.fasta \
+#    -n ltde_seqs_test -w /Users/WRShoemaker/GitHub/LTDE/data/tree
 
-# for the protein tree..........
+# test for the protein tree..........
 raxmlHPC-PTHREADS -T 4 -f a -m PROTGAMMALG -p 12345 -x 12345 -# autoMRE \
-    -s \
-    -n \
-    -w
+    -s /Users/WRShoemaker/GitHub/LTDE/data/align/ribosomal_protein_seqs_align_concat.fa \
+    -n test_protein \
+    -w /Users/WRShoemaker/GitHub/LTDE/data/align
+
+raxmlHPC-PTHREADS -T 4 -f a -m PROTGAMMALG -p 12345 -x 12345 -# autoMRE \
+    -s /N/dc2/projects/muri2/Task2/LTDE/data/align/ribosomal_protein_seqs_align_concat.fa \
+    -n protein_tree \
+    -w /N/dc2/projects/muri2/Task2/LTDE/data/tree
+
 
 # -T = number of threads
 # -f = specifies bootstrapping algorithm with ML generating tree at same time
