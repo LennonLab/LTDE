7a7d026ee3908f7a1b344bcce505b9393a689956
Will Shoemaker <shoemakah@gmail.com>
2018-06-22 21:44:33 -0400

    fig1

--- Python/ltde_tools.py
+++ Python/ltde_tools.py
@@ -1,10 +1,31 @@
 import os, math
+import pandas as pd
 
 def get_path():
     return os.path.expanduser("~/GitHub/LTDE")
 
+def clean_demography_df(df):
+    df.loc[((df["strain"] == 'KBS0711W') & (df["rep"] == 1)), 'rep'] = 5
+    df.loc[((df["strain"] == 'KBS0711W') & (df["rep"] == 2)), 'rep'] = 6
+    df.loc[((df["strain"] == 'KBS0711W') & (df["rep"] == 3)), 'rep'] = 7
+    df.loc[((df["strain"] == 'KBS0711W') & (df["rep"] == 4)), 'rep'] = 8
+    df.loc[(df["strain"] == 'KBS0711W'), 'strain'] = 'KBS0711'
+    return df
+
+
+def get_strain_genus_dict():
+    df_path = get_path() + '/data/traits/traits.txt'
+    df = pd.read_csv(df_path, sep = '\t')
+    genus_dict = pd.Series(df.Genus.values,index=df.Code).to_dict()
+    genus_dict['KBS0727'] = 'Bradyrhizobium'
+    return genus_dict
+
 def weibull_mean(alpha, beta):
     return beta * math.gamma(1 + (1/alpha))
+
+def weibull_half_life(alpha, beta):
+    #return beta * math.gamma(1 + (1/alpha))
+    return beta *  ((-math.log(0.5)) ** (1/alpha))
 
 def weibull_variance(alpha, beta):
     return (beta ** 2) * (math.gamma(1 + (2/alpha)) -  (math.gamma(1 + (1/alpha)) ** 2))
@@ -22,6 +43,18 @@ def weibull_CIs(mean, sd, n, lower = True, pooled = False):
     else:
         CI = math.exp( math.log(mean) + (1.96 * se_g1))
     return CI
+
+
+def get_mean_time_death():
+    df_path = get_path() + '/data/demography/weibull_results.csv'
+    df = pd.read_csv(df_path, sep = ',', index_col = 0)
+    df['mean_days_death'] = df.apply(lambda row: weibull_mean(alpha = row['alpha'], beta = row['beta']), axis=1)
+    df['sd_days_death'] = df.apply(lambda row: math.sqrt(weibull_variance(alpha = row['alpha'], beta = row['beta'])), axis=1)
+    df['CI025_mean_days_death'] = df.apply(lambda row: weibull_CIs(mean = row['mean_days_death'], sd = row['sd_days_death'], n= row['N.obs'], lower = True ) , axis=1)
+    df['CI975_mean_days_death'] = df.apply(lambda row: weibull_CIs(mean = row['mean_days_death'], sd = row['sd_days_death'], n= row['N.obs'], lower = False ) , axis=1)
+    df['half_life'] = df.apply(lambda row: weibull_half_life(alpha = row['alpha'], beta = row['beta']), axis=1)
+    return df
+
 
 
 class classFASTA:
