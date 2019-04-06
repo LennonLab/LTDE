f4cad8c08045b5cd41db5fa30500e00a480b6a80
Will Shoemaker <shoemakah@gmail.com>
2019-04-06 15:09:25 -0400

    neighbot 16S

--- Python/clean_data.py
+++ Python/clean_data.py
@@ -3,6 +3,103 @@ import ltde_tools as lt
 import glob, re, os, subprocess, math
 import pandas as pd
 
+
+def make_16S_fata():
+
+    alignments = ['KBS0710_NR_024911', 'KBS0721_NR_114994']
+
+    def generate_16S_consenus(alignment):
+        mpileup = get_path() + '/data/align/' + alignment + '.pileup'
+        fasta =  get_path() + '/data/align/ltde_seqs.fasta'
+        out_fasta = open(fasta,'a+')
+        strain = alignment.split('_')[0]
+        seq = []
+        with open(mpileup, "r") as file:
+            array = []
+            for line in file:
+                line_split = line.split()
+                if 'KBS0710' in mpileup:
+                    exclude = range(436, 456) + range(1,30) + range(1519, 1523)
+                elif 'KBS0721' in mpileup:
+                    exclude = range(162, 186) + [1, 2, 1501]
+
+                if int(line_split[1]) in exclude:
+                    continue
+                else:
+                    ref = line_split[2]
+                    # remove lower case
+                    align_site = [x.upper() for x in line_split[4]]
+                    coverage = len(align_site)
+                    A_count = align_site.count('A') / coverage
+                    C_count = align_site.count('C') / coverage
+                    G_count = align_site.count('G') / coverage
+                    T_count = align_site.count('T') / coverage
+                    nucs = ['A', 'C', 'G', 'T']
+                    nuc_freqs = [A_count, C_count, G_count, T_count]
+                    nuc_freq_max = max(nuc_freqs)
+                    nuc_freq_max_index = nuc_freqs.index(nuc_freq_max)
+                    nuc_max = nucs[nuc_freq_max_index]
+                    if nuc_freq_max < 0.5:
+                        seq.extend(ref)
+                    else:
+                        seq.extend(nuc_max)
+
+        seq_str = ''.join(seq)
+        out_fasta.write('\n')
+        out_fasta.write('>' + strain + '\n')
+        split_seq = ltde_tools.split_by_n(seq_str, 60)
+        for split_seq_i in split_seq:
+            out_fasta.write(split_seq_i + '\n')
+        out_fasta.close()
+
+
+
+    def remove_dups():
+        class_fa = ltde_tools.classFASTA(get_path()+'/data/align/ltde_neighbors/merged_neighbors.txt')
+        out_fasta = open(get_path()+'/data/align/ltde_neighbors/merged_neighbors_no_dups.txt', 'w+')
+
+        read_fa = class_fa.readFASTA()
+        names = [x[0] for x in read_fa]
+        def duplicates(lst, item):
+            return [i for i, x in enumerate(lst) if x == item]
+        dups = dict((x, duplicates(names, x)) for x in set(names) if names.count(x) > 1)
+        to_remove = []
+        for k, v in sorted(dups.items()):
+            to_remove.extend(v[1:])
+        read_fa_no_dups = [i for j, i in enumerate(read_fa) if j not in to_remove]
+
+        for x in read_fa_no_dups:
+
+            name = x[0]
+            out_fasta.write('>' + name + '\n')
+            seq = x[1]
+            n = 80
+            #seq_split = [seq[i:i+n] for i in range(0, len(seq), n)]
+            seq_split = lt.split_by_n(seq, n)
+            for seq_i in seq_split:
+                out_fasta.write(seq_i + '\n')
+            out_fasta.write('\n')
+        out_fasta.close()
+
+
+    def run_alignments():
+        for align in alignments:
+            generate_16S_consenus(align)
+
+    # un-blank the commands to merge the sequence data
+    #os.system('cat ~/GitHub/LTDE/data/align/ltde_neighbors/*.txt > ~/GitHub/LTDE/data/align/ltde_neighbors/merged_neighbors.txt')
+    #remove_dups()
+    #os.system('cat ~/GitHub/LTDE/data/align/ltde_seqs.fasta ~/GitHub/LTDE/data/align/ltde_neighbors/merged_neighbors_no_dups.txt > ~/GitHub/LTDE/data/align/ltde_neighbors_seqs.fasta')
+
+    # https://www.arb-silva.de/aligner/job/632523
+    # NC_005042.1:353331-354795 renamed as NC_005042.1.353331-354795
+    os.system(sed -i -e 's/NC_005042.1:353331-354795/NC_005042.1.353331-354795/g' ~/GitHub/LTDE/data/align/ltde_neighbors_seqs.fasta)
+
+    # ltde_neighbors_seqs.fasta uploaded to ARB and aligned
+    # alignment file = arb-silva.de_2019-04-06_id632523.fasta
+
+
+
 def get_iRep():
     directory = os.fsencode(lt.get_path() + '/data/bwa_sam')
     for file in os.listdir(directory):
@@ -96,4 +193,4 @@ def clean_COGs():
 
 
 #clean_iRep()
-clean_COGs()
+#clean_COGs()
--- Python/ltde_tools.py
+++ Python/ltde_tools.py
@@ -1,16 +1,7 @@
 import os, math
-import pandas as pd
 
 def get_path():
     return os.path.expanduser("~/GitHub/LTDE")
-
-def clean_demography_df(df):
-    df.loc[((df["strain"] == 'KBS0711W') & (df["rep"] == 1)), 'rep'] = 5
-    df.loc[((df["strain"] == 'KBS0711W') & (df["rep"] == 2)), 'rep'] = 6
-    df.loc[((df["strain"] == 'KBS0711W') & (df["rep"] == 3)), 'rep'] = 7
-    df.loc[((df["strain"] == 'KBS0711W') & (df["rep"] == 4)), 'rep'] = 8
-    df.loc[(df["strain"] == 'KBS0711W'), 'strain'] = 'KBS0711'
-    return df
 
 
 def get_strain_genus_dict():
@@ -20,45 +11,7 @@ def get_strain_genus_dict():
     genus_dict['KBS0727'] = 'Bradyrhizobium'
     return genus_dict
 
-def weibull_mean(alpha, beta):
-    return beta * math.gamma(1 + (1/alpha))
 
-def weibull_half_life(alpha, beta):
-    #return beta * math.gamma(1 + (1/alpha))
-    return beta *  ((-math.log(0.5)) ** (1/alpha))
-
-def weibull_variance(alpha, beta):
-    return (beta ** 2) * (math.gamma(1 + (2/alpha)) -  (math.gamma(1 + (1/alpha)) ** 2))
-
-def weibull_CIs(mean, sd, n, lower = True, pooled = False):
-    # alpha of 0.05, so z_(alpha/2) = 1.96
-    if pooled == True:
-        # pass list of sample sizes
-        n_pooled = math.sqrt( sum(1 / n) )
-        se_g1 = (sd * n_pooled) / mean
-    else:
-        se_g1 = (sd / math.sqrt(n)) / mean
-    if lower == True:
-        CI = math.exp( math.log(mean) - (1.96 * se_g1))
-    else:
-        CI = math.exp( math.log(mean) + (1.96 * se_g1))
-    return CI
-
-
-def get_mean_time_death():
-    df_path = get_path() + '/data/demography/weibull_results.csv'
-    df = pd.read_csv(df_path, sep = ',', index_col = 0)
-    df = clean_demography_df(df)
-    df['mean_days_death'] = df.apply(lambda row: weibull_mean(alpha = row['alpha'], beta = row['beta']), axis=1)
-    df['sd_days_death'] = df.apply(lambda row: math.sqrt(weibull_variance(alpha = row['alpha'], beta = row['beta'])), axis=1)
-    df['CI025_mean_days_death'] = df.apply(lambda row: weibull_CIs(mean = row['mean_days_death'], sd = row['sd_days_death'], n= row['N.obs'], lower = True ) , axis=1)
-    df['CI975_mean_days_death'] = df.apply(lambda row: weibull_CIs(mean = row['mean_days_death'], sd = row['sd_days_death'], n= row['N.obs'], lower = False ) , axis=1)
-    df['half_life'] = df.apply(lambda row: weibull_half_life(alpha = row['alpha'], beta = row['beta']), axis=1)
-
-    out_path = get_path() + '/data/demography/weibull_results_half_life.txt'
-    df.to_csv(out_path, sep = '\t',  index = True)
-
-    return df
 
 
 
