df85809dce915ecf0ab136c85251147e27b71bea
Will Shoemaker <shoemakah@gmail.com>
2019-04-26 01:17:40 -0400

    init assembly code

--- bash/get_illumina_reads.sh
+++ bash/get_illumina_reads.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 #PBS -k o
-#PBS -l nodes=1:ppn=8,vmem=100gb,walltime=12:00:00
+#PBS -l nodes=1:ppn=8,vmem=100gb,walltime=4:00:00
 #PBS -M wrshoema@iu.edu
 #PBS -m abe
 #PBS -j oe
 
-module unload python
-module load python/3.6.1
+#module unload python
+#module load python/3.6.1
+module load python/2.7.13
 module load fastqc
-module load cutadapt
+module load cutadapt/intel/1.16
 
 declare -a strains=("ATCC13985" "ATCC43928" "KBS0701" "KBS0702" "KBS0703" \
                     "KBS0705" "KBS0706" "KBS0707" "KBS0710" "KBS0711" \
@@ -28,14 +29,15 @@ declare -a libraries=("ATCC13985_D400_100" "ATCC43928_D400_100" \
                       "KBS0801_B_2015_SoilGenomes" "KBS0801_D400_100" \
                       "KBS0802_2015_SoilGenomes" "KBS0812_D400_100")
 
-for folder in /N/dc2/projects/muri2/Task2/LTDE/illumina_data/*/
-do
-  echo $folder
-done
+#for folder in /N/dc2/projects/muri2/Task2/LTDE/illumina_data/*/
+#do
+#  echo $folder
+#done
 
 for folder in /N/dc2/projects/muri2/Task2/LTDE/illumina_data/*/
 do
   if [[ $folder == *"D400_100"* ]]; then
+    continue
     sample_sheet="${folder}SampleSheet.csv"
     adapt1="$(sed '2q;d' ${sample_sheet} | cut -d"," -f5 | cut -d"-" -f1)"
     adapt2="$(sed '2q;d' ${sample_sheet} | cut -d"," -f5 | cut -d"-" -f2)"
@@ -43,80 +45,66 @@ do
     R2="${folder}"*"_R2_001.fastq.gz"
     zgrep ^@HSQ $R1 | cut -d":" -f10 | sort -r | uniq > "${folder}"barcodes_R1.txt
     zgrep ^@HSQ $R2 | cut -d":" -f10 | sort -r | uniq > "${folder}"barcodes_R2.txt
-    cat "${folder}barcodes_R"?".txt" >> "${folder}barcodes.txt"
-    sort -u "${folder}barcodes.txt" >> "${folder}barcodes_unique.txt"
+    cat "${folder}barcodes_R"?".txt" > "${folder}barcodes.txt"
+    #sort -u "${folder}barcodes.txt" >> "${folder}barcodes_unique.txt"
+    sort "${folder}barcodes.txt" | uniq > "${folder}barcodes_unique.txt"
     python /N/dc2/projects/muri2/Task2/LTDE/bash/add_fasta_headers.py "${folder}barcodes_unique.txt" "${folder}barcodes_unique.fa"
 
-    R1_name="$( echo "${R1}" | cut -d "." -f1-1 )"
-    R2_name="$( echo "${R2}" | cut -d "." -f1-1 )"
+    R1_name="$( echo $R1 | cut -d "." -f1-1 )"
+    R2_name="$( echo $R2 | cut -d "." -f1-1 )"
 
-    cutadapt -q 30,30 -a $adapt1 -A $adapt2 -g file:"${folder}barcodes_unique.fa" \
+    cutadapt -q 30,30 -u 20 -a $adapt1 -A $adapt2 -g file:"${folder}barcodes_unique.fa" \
               --minimum-length 20 -o "${R1_name}_clean.fastq.gz" \
               -p "${R2_name}_clean.fastq.gz" $R1 $R2
 
-
-
   elif [[ $folder == *"_2015_SoilGenomes"* ]]; then
+    continue
     adapt1="AGATCGGAAGAGC"
     adapt2="AGATCGGAAGAGC"
     R1="${folder}"*"_R1.fastq"
     R2="${folder}"*"_R2.fastq"
-    # make sure this works
-    grep ^@HSQ $R1 | cut -d":" -f10 | sort -r | uniq > "${folder}"barcodes_R1.txt
-    grep ^@HSQ $R2 | cut -d":" -f10 | sort -r | uniq > "${folder}"barcodes_R2.txt
+
+    grep ^@HWI $R1 | cut -d":" -f10 | sort -r | uniq > "${folder}"barcodes_R1.txt
+    grep ^@HWI $R2 | cut -d":" -f10 | sort -r | uniq > "${folder}"barcodes_R2.txt
     cat "${folder}barcodes_R"?".txt" >> "${folder}barcodes.txt"
     sort -u "${folder}barcodes.txt" >> "${folder}barcodes_unique.txt"
     python /N/dc2/projects/muri2/Task2/LTDE/bash/add_fasta_headers.py "${folder}barcodes_unique.txt" "${folder}barcodes_unique.fa"
 
-    R1_name="$( echo "${R1}" | cut -d "." -f1-1 )"
-    R2_name="$( echo "${R2}" | cut -d "." -f1-1 )"
+    R1_name="$( echo $R1 | cut -d "." -f1-1 )"
+    R2_name="$( echo $R2 | cut -d "." -f1-1 )"
 
-    cutadapt -q 30,30 -a $adapt1 -A $adapt2 -g file:"${folder}barcodes_unique.fa" \
+    cutadapt -q 30,30 -u 20 -a $adapt1 -A $adapt2 -g file:"${folder}barcodes_unique.fa" \
               --minimum-length 20 -o "${R1_name}_clean.fastq" \
               -p "${R2_name}_clean.fastq" $R1 $R2
 
   elif [[ $folder == *"_GSF911"* ]]; then
-    echo "What"
-    # use GSF911_adapters.txt
-    # cutadapt -a file:/N/dc2/projects/muri2/Task2/LTDE/bash/GSF911_adapters.fasta
+    #continue
+    R1="${folder}"*"_R1_001.fastq.gz"
+    R2="${folder}"*"_R2_001.fastq.gz"
+    # no barcode listed, remove overrepresented sequences after
+    #zgrep ^@M01529 $R1 | cut -d":" -f10 | sort -r | uniq > "${folder}"barcodes_R1.txt
+    #zgrep ^@M01529 $R2 | cut -d":" -f10 | sort -r | uniq > "${folder}"barcodes_R2.txt
 
-
+    #M01529
+    R1_name="$( echo $R1 | cut -d "." -f1-1 )"
+    R2_name="$( echo $R2 | cut -d "." -f1-1 )"
+    adapt=/N/dc2/projects/muri2/Task2/LTDE/bash/GSF911_adapters.fasta
+    cutadapt -q 30,30 -u 20 -a file:"${adapt}" \
+              --minimum-length 20 -o "${R1_name}_clean.fastq" \
+              -p "${R2_name}_clean.fastq" $R1 $R2
   else
-    echo "Unknown file"
+    continue
 
+  #fastqc $R1 > "${folder}"R1_results.out
+  #fastqc $R2 > "${folder}"R2_results.out
 
-  fastqc "$R1" --outdir="${folder}R1_fastqc"
-  fastqc "$R2" --outdir="${folder}R2_fastqc"
-
-  fastqc "${R1_name}_clean.fastq.gz" --outdir="${R1_name}_clean"
-  fastqc "${R2_name}_clean.fastq.gz" --outdir="${R2_name}_clean"
-
+  #fastqc "${R1_name}_clean.fastq.gz" > "${folder}"R1_clean_results.out
+  #fastqc "${R2_name}_clean.fastq.gz" > "${folder}"R2_clean_results.out
   fi
 done
 
 
-
+#fastqc Ev712_TAAGGCGAATCT-TATCCTCT_L002_R1_001_clean.fastq.gz > KBS0712_R1_clean_results.out
 
 
 #zcat Ev13483_TAAGGCGAATCT-AAGGAGTA_L002_R1_001.fastq.gz | head -n 1
-
-
-
-
-# run quast after assembly
-
-
-
-
-
-#fastqc $R1 >> results.out
-#fastqc $R2 >> results.out
-
-#cutadapt -q 10 -a AGATCGGAAGAGC --minimum-length 20 -o ./tmp.1.fastq -p ./tmp.2.fastq ./$R1 ./$R2
-#cutadapt -q 10 -a AGATCGGAAGAGC --minimum-length 20 -o ./$GENOME.R2.trim.fastq -p ./$GENOME.R1.trim.fastq ./tmp.2.fastq ./tmp.1.fastq
-#rm ./tmp.1.fastq ./tmp.2.fastq
-#interleave-reads.py ./$GENOME.R1.trim.fastq ./$GENOME.R2.trim.fastq -o ./$GENOME.trim.interleaved.fastq
-#fastq_quality_filter -Q33 -q 30 -p 50 -i ./$GENOME.trim.interleaved.fastq > ./$GENOME.trim.interleaved.trim.fastq
-#extract-paired-reads.py ./$GENOME.trim.interleaved.trim.fastq
-#normalize-by-median.py -k 25 -C 25 -N 4 -x 2e9 -p ./$GENOME.trim.interleaved.trim.fastq.pe
-#extract-paired-reads.py ./$GENOME.trim.interleaved.trim.fastq.pe.keep
--- bash/nanopore.sh
+++ bash/nanopore.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 #PBS -k o
-#PBS -l nodes=1:ppn=8,vmem=100gb,walltime=72:00:00
+#PBS -l nodes=1:ppn=8,vmem=100gb,walltime=02:00:00
 #PBS -M wrshoema@iu.edu
 #PBS -m abe
 #PBS -j oe
 
+module load fastqc
+module unload python
+module load python/3.6.1
 # base-calling with Guppy
 
 # FLO-MIN106
@@ -26,7 +29,6 @@ out_dir_run2=/N/dc2/projects/muri2/Task2/LTDE/data/nanopore_basecalled/run2
 fast5_dir_run3=/N/dc2/projects/muri2/Task2/LTDE/GSF2099/GSF2099-Lennon-run3-20190408/GSF2099-set3/20190408_2058_MN22062_FAK70247_d34ce651/fast5_pass
 out_dir_run3=/N/dc2/projects/muri2/Task2/LTDE/data/nanopore_basecalled/run3
 
-#fast5_dir_run2=/N/dc2/projects/muri2/Task2/LTDE/GSF2099/test
 
 #/N/dc2/projects/muri2/Task2/LTDE/ont-guppy-cpu/bin/guppy_basecaller \
 #    -i $fast5_dir_run2 \
@@ -51,10 +53,10 @@ out_dir_bc_run2=/N/dc2/projects/muri2/Task2/LTDE/data/nanopore_basecalled_bc/run
 out_dir_bc_run3=/N/dc2/projects/muri2/Task2/LTDE/data/nanopore_basecalled_bc/run3
 
 bc_cfg=/N/dc2/projects/muri2/Task2/LTDE/ont-guppy-cpu/data/barcoding/configuration.cfg
-/N/dc2/projects/muri2/Task2/LTDE/ont-guppy-cpu/bin/guppy_barcoder \
-    --input_path $out_dir_run2 \
-    --save_path $out_dir_bc_run2 \
-    --config $bc_cfg
+#/N/dc2/projects/muri2/Task2/LTDE/ont-guppy-cpu/bin/guppy_barcoder \
+#    --input_path $out_dir_run2 \
+#    --save_path $out_dir_bc_run2 \
+#    --config $bc_cfg
 
 
 #/N/dc2/projects/muri2/Task2/LTDE/ont-guppy-cpu/bin/guppy_barcoder \
@@ -62,10 +64,55 @@ bc_cfg=/N/dc2/projects/muri2/Task2/LTDE/ont-guppy-cpu/data/barcoding/configurati
 #    --save_path $out_dir_bc_run3
 #    --config $bc_cfg
 
+# merge files
 
-# run fastqc on each nanopore file
+data_path=/N/dc2/projects/muri2/Task2/LTDE/data
 
-# clean up, do I need porechop?
+#cat "${data_path}/nanopore_basecalled_bc/run2/barcode01/fastq_runid_"*".fastq" > "${data_path}/nanopore_basecalled_bc_merged/KBS0710.fastq"
+#cat "${data_path}/nanopore_basecalled_bc/run2/barcode02/fastq_runid_"*".fastq" > "${data_path}/nanopore_basecalled_bc_merged/KBS0711.fastq"
+#cat "${data_path}/nanopore_basecalled_bc/run2/barcode03/fastq_runid_"*".fastq" > "${data_path}/nanopore_basecalled_bc_merged/KBS0712.fastq"
+#cat "${data_path}/nanopore_basecalled_bc/run2/barcode04/fastq_runid_"*".fastq" > "${data_path}/nanopore_basecalled_bc_merged/KBS0713.fastq"
+#cat "${data_path}/nanopore_basecalled_bc/run2/barcode05/fastq_runid_"*".fastq" > "${data_path}/nanopore_basecalled_bc_merged/KBS0714.fastq"
+#cat "${data_path}/nanopore_basecalled_bc/run2/barcode06/fastq_runid_"*".fastq" > "${data_path}/nanopore_basecalled_bc_merged/KBS0715.fastq"
+#cat "${data_path}/nanopore_basecalled_bc/run2/barcode07/fastq_runid_"*".fastq" > "${data_path}/nanopore_basecalled_bc_merged/KBS0721.fastq"
+#cat "${data_path}/nanopore_basecalled_bc/run2/barcode08/fastq_runid_"*".fastq" > "${data_path}/nanopore_basecalled_bc_merged/KBS0722.fastq"
 
-# clean fasta reads
+cat "${data_path}/nanopore_basecalled_bc/run3/barcode01/fastq_runid_"*".fastq" > "${data_path}/nanopore_basecalled_bc_merged/ATCC13985.fastq"
+cat "${data_path}/nanopore_basecalled_bc/run3/barcode02/fastq_runid_"*".fastq" > "${data_path}/nanopore_basecalled_bc_merged/ATCC43928.fastq"
+cat "${data_path}/nanopore_basecalled_bc/run3/barcode03/fastq_runid_"*".fastq" > "${data_path}/nanopore_basecalled_bc_merged/KBS0701.fastq"
+cat "${data_path}/nanopore_basecalled_bc/run3/barcode04/fastq_runid_"*".fastq" > "${data_path}/nanopore_basecalled_bc_merged/KBS0702.fastq"
+cat "${data_path}/nanopore_basecalled_bc/run3/barcode05/fastq_runid_"*".fastq" > "${data_path}/nanopore_basecalled_bc_merged/KBS0703.fastq"
+cat "${data_path}/nanopore_basecalled_bc/run3/barcode06/fastq_runid_"*".fastq" > "${data_path}/nanopore_basecalled_bc_merged/KBS0705.fastq"
+cat "${data_path}/nanopore_basecalled_bc/run3/barcode07/fastq_runid_"*".fastq" > "${data_path}/nanopore_basecalled_bc_merged/KBS0707.fastq"
+cat "${data_path}/nanopore_basecalled_bc/run3/barcode08/fastq_runid_"*".fastq" > "${data_path}/nanopore_basecalled_bc_merged/KBS0724.fastq"
+
+#cat "${data_path}/nanopore_basecalled_bc/run4/barcode01/fastq_runid_"*".fastq" > "${data_path}/nanopore_basecalled_bc_merged/KBS0725.fastq"
+#cat "${data_path}/nanopore_basecalled_bc/run4/barcode02/fastq_runid_"*".fastq" > "${data_path}/nanopore_basecalled_bc_merged/KBS0727.fastq"
+#cat "${data_path}/nanopore_basecalled_bc/run4/barcode03/fastq_runid_"*".fastq" > "${data_path}/nanopore_basecalled_bc_merged/KBS0801.fastq"
+#cat "${data_path}/nanopore_basecalled_bc/run4/barcode04/fastq_runid_"*".fastq" > "${data_path}/nanopore_basecalled_bc_merged/KBS0802.fastq"
+#cat "${data_path}/nanopore_basecalled_bc/run4/barcode05/fastq_runid_"*".fastq" > "${data_path}/nanopore_basecalled_bc_merged/KBS0812.fastq"
+#cat "${data_path}/nanopore_basecalled_bc/run4/barcode06/fastq_runid_"*".fastq" > "${data_path}/nanopore_basecalled_bc_merged/KBS0706.fastq"
+#cat "${data_path}/nanopore_basecalled_bc/run4/barcode07/fastq_runid_"*".fastq" > "${data_path}/nanopore_basecalled_bc_merged/PHB12.fastq"
+
+
+
+#fastqc "${data_path}/nanopore_basecalled_bc_merged/KBS0710.fastq" > "${data_path}/nanopore_basecalled_bc_merged/KBS0710_results.out"
+#fastqc "${data_path}/nanopore_basecalled_bc_merged/KBS0711.fastq" > "${data_path}/nanopore_basecalled_bc_merged/KBS0711_results.out"
+#fastqc "${data_path}/nanopore_basecalled_bc_merged/KBS0712.fastq" > "${data_path}/nanopore_basecalled_bc_merged/KBS0712_results.out"
+#fastqc "${data_path}/nanopore_basecalled_bc_merged/KBS0713.fastq" > "${data_path}/nanopore_basecalled_bc_merged/KBS0713_results.out"
+#fastqc "${data_path}/nanopore_basecalled_bc_merged/KBS0721.fastq" > "${data_path}/nanopore_basecalled_bc_merged/KBS0721_results.out"
+#fastqc "${data_path}/nanopore_basecalled_bc_merged/KBS0722.fastq" > "${data_path}/nanopore_basecalled_bc_merged/KBS0722_results.out"
+#fastqc "${data_path}/nanopore_basecalled_bc_merged/KBS0714.fastq" > "${data_path}/nanopore_basecalled_bc_merged/KBS0714_results.out"
+#fastqc "${data_path}/nanopore_basecalled_bc_merged/KBS0715.fastq" > "${data_path}/nanopore_basecalled_bc_merged/KBS0715_results.out"
+
+NanoFilt=/N/u/wrshoema/Carbonate/.local/lib/python3.6/site-packages/nanofilt/NanoFilt.py
+for filename in "${data_path}/nanopore_basecalled_bc_merged/"*".fastq"; do
+  if [[ $filename != *"_clean.fastq" ]]; then
+    strain="$( echo $filename | rev | cut -d/ -f1 | rev | cut -d. -f1 )"
+    #fastqc $filename > "${data_path}/nanopore_basecalled_bc_merged/${strain}_results.out"
+    python $NanoFilt -q 10 -l 500 --headcrop 100 $filename > "${data_path}/nanopore_basecalled_bc_merged/${strain}_clean.fastq"
+    #fastqc "${data_path}/nanopore_basecalled_bc_merged/${strain}_clean.fastq" > "${data_path}/nanopore_basecalled_bc_merged/${strain}_clean_results.out"
+  fi
+done
+
 # then assemble with spades
