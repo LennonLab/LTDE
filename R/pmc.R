9d40b75e09044272773087fe3b69031c573c531f
Will Shoemaker <shoemakah@gmail.com>
2019-06-13 16:54:01 -0400

    pmc add summary file

--- R/pmc.R
+++ R/pmc.R
@@ -33,108 +33,66 @@ is.ultrametric(ml.rooted)
 ml.rooted.um  <- chronos(ml.rooted)
 is.ultrametric(ml.rooted.um)
 
-mttf <- df$mttf.log10
-names(mttf) <- df$Species
+beta.log10 <- df$beta.log10
+names(beta.log10) <- df$Species
 alpha <- df$alpha
 names(alpha) <- df$Species
 
-test.mttf <- pmc(ml.rooted.um, mttf, "lambda", "OU", nboot = 100)
-test.alpha <- pmc(ml.rooted.um, alpha, "lambda", "OU", nboot = 100)
-
-length(test.mttf$null[test.mttf$null > test.mttf$lr]) /100
-length(test.alpha$null[test.alpha$null > test.alpha$lr]) /100
-
-
-dists <- data.frame(null = test.mttf$null, test = test.mttf$test)
-
-dists %>% 
-  gather(dist, value) %>%
-  ggplot(aes(value, fill = dist)) + 
-  geom_density(alpha = 0.5) + 
-  geom_vline(xintercept = test.mttf$lr)
-
-length(dists$null[dists$null > test.mttf$lr]) /100
-
-
 
 iter <- 1000
-BM.OU.mttf <- pmc(ml.rooted.um, mttf, "BM", "OU", nboot = iter)
+BM.OU.beta.log10 <- pmc(ml.rooted.um, beta.log10, "BM", "OU", nboot = iter)
 BM.OU.alpha <- pmc(ml.rooted.um, alpha, "BM", "OU", nboot = iter)
-BM.GBM.mttf <- pmc(ml.rooted.um, mttf, "BM", "trend", nboot = iter)
+BM.GBM.beta.log10 <- pmc(ml.rooted.um, beta.log10, "BM", "trend", nboot = iter)
 BM.GBM.alpha <- pmc(ml.rooted.um, alpha, "BM", "trend", nboot = iter)
 # run after this
-OU.GBM.mttf <- pmc(ml.rooted.um, mttf, "OU", "trend", nboot = iter)
-OU.GBM.alpha <- pmc(ml.rooted.um, alpha, "OU", "trend", nboot = iter)
+GBM.OU.beta.log10 <- pmc(ml.rooted.um, beta.log10, "trend", "OU", nboot = iter)
+GBM.OU.alpha <- pmc(ml.rooted.um, alpha, "trend", "OU", nboot = iter)
 
 
-mttf.ll <- do.call(c, list(BM.OU.mttf$null, BM.OU.mttf$test, BM.GBM.mttf$null, BM.GBM.mttf$test, OU.GBM.mttf$null, OU.GBM.mttf$test))
-mttf.test <- do.call(c, list(replicate(iter*2, "BM_OU"), replicate(iter*2, "BM_GBM"), replicate(iter*2, "OU_GBM")))
-mttf.model <- do.call(c, list(replicate(iter, "BM"), replicate(iter, "OU"), replicate(iter, "BM"), replicate(iter, "GBM"), replicate(iter, "OU"), replicate(iter, "GBM")))
-mttf.df <- data.frame(llr = mttf.ll, test= mttf.test, model = mttf.model)
+#beta.log10.ll <- do.call(c, list(BM.OU.beta.log10$null, BM.OU.beta.log10$test, BM.GBM.beta.log10$null, BM.GBM.beta.log10$test, GBM.OU.beta.log10$null, GBM.OU.beta.log10$test))
+#beta.log10.test <- do.call(c, list(replicate(iter*2, "BM_OU"), replicate(iter*2, "BM_GBM"), replicate(iter*2, "GBM_OU")))
+#beta.log10.model <- do.call(c, list(replicate(iter, "BM"), replicate(iter, "OU"), replicate(iter, "BM"), replicate(iter, "GBM"), replicate(iter, "OU"), replicate(iter, "GBM")))
+#beta.log10.df <- data.frame(llr = mttf.ll, test= mttf.test, model = mttf.model)
 
-alpha.ll <- do.call(c, list(BM.OU.alpha$null, BM.OU.alpha$test, BM.GBM.alpha$null, BM.GBM.alpha$test, OU.GBM.alpha$null, OU.GBM.alpha$test))
-alpha.test <- do.call(c, list(replicate(iter*2, "BM_OU"), replicate(iter*2, "BM_GBM"), replicate(iter*2, "OU_GBM")))
-alpha.model <- do.call(c, list(replicate(iter, "BM"), replicate(iter, "OU"), replicate(iter, "BM"), replicate(iter, "GBM"), replicate(iter, "OU"), replicate(iter, "GBM")))
-alpha.df <- data.frame(llr = alpha.ll, test= alpha.test, model = alpha.model)
+#alpha.ll <- do.call(c, list(BM.OU.alpha$null, BM.OU.alpha$test, BM.GBM.alpha$null, BM.GBM.alpha$test, GBM.OU.alpha$null, GBM.OU.alpha$test))
+#alpha.test <- do.call(c, list(replicate(iter*2, "BM_OU"), replicate(iter*2, "BM_GBM"), replicate(iter*2, "OU_GBM")))
+#alpha.model <- do.call(c, list(replicate(iter, "BM"), replicate(iter, "OU"), replicate(iter, "BM"), replicate(iter, "GBM"), replicate(iter, "OU"), replicate(iter, "GBM")))
+#alpha.df <- data.frame(llr = alpha.ll, test= alpha.test, model = alpha.model)
 
-write.csv(mttf.df, file = "data/pmc/pmc_mttf.csv")
-write.csv(alpha.df, file = "data/pmc/pmc_alpha.csv")
+#write.csv(mttf.df, file = "data/pmc/pmc_beta_log10.csv")
+#write.csv(alpha.df, file = "data/pmc/pmc_alpha.csv")
 
-df.summary <- cbind(parameter=c("mttf","mttf","mttf","alpha","alpha","alpha"), 
-                    test=c("BM_OU","BM_GBM", "OU_GBM", "BM_OU","BM_GBM", "OU_GBM"), 
-                    llr=c(BM.OU.mttf$lr, BM.GBM.mttf$lr, OU.GBM.mttf$lr, BM.OU.alpha$lr, BM.GBM.alpha$lr, OU.GBM.alpha$lr))
+p_value.BM.OU.beta.log10 <- length(BM.OU.beta.log10$null[BM.OU.beta.log10$null < BM.OU.beta.log10$lr] ) / iter
+p_value.BM.GBM.beta.log10 <- length(BM.GBM.beta.log10$null[BM.GBM.beta.log10$null < BM.GBM.beta.log10$lr] ) / iter
+p_value.GBM.OU.beta.log10 <- length(GBM.OU.beta.log10$null[GBM.OU.beta.log10$null < GBM.OU.beta.log10$lr] ) / iter
+
+p_value.BM.OU.alpha <- length(BM.OU.alpha$null[BM.OU.alpha$null < BM.OU.alpha$lr] ) / iter
+p_value.BM.GBM.alpha <- length(BM.GBM.alpha$null[BM.GBM.alpha$null < BM.GBM.alpha$lr] ) / iter
+p_value.GBM.OU.alpha <- length(GBM.OU.alpha$null[GBM.OU.alpha$null < GBM.OU.alpha$lr] ) / iter
+
+
+df.summary <- cbind(parameter=c("beta.log10","beta.log10","beta.log10","alpha","alpha","alpha"), 
+                    test=c("BM_OU","BM_GBM", "GBM_OU", "BM_OU","BM_GBM", "GBM_OU"), 
+                    llr=c(BM.OU.beta.log10$lr, BM.GBM.beta.log10$lr, GBM.OU.beta.log10$lr, BM.OU.alpha$lr, BM.GBM.alpha$lr, GBM.OU.alpha$lr),
+                    p_value=c(p_value.BM.OU.beta.log10, p_value.BM.GBM.beta.log10, p_value.GBM.OU.beta.log10, p_value.BM.OU.alpha, p_value.BM.GBM.alpha, p_value.GBM.OU.alpha),
+                    sigsq_BM=c(BM.OU.beta.log10$A$opt$sigsq, BM.GBM.beta.log10$A$opt$sigsq, NA, BM.OU.alpha$A$opt$sigsq, BM.GBM.alpha$A$opt$sigsq, NA),
+                    sigsq_OU=c(BM.OU.beta.log10$B$opt$sigsq, NA, GBM.OU.beta.log10$B$opt$sigsq, BM.OU.alpha$B$opt$sigsq, NA,  GBM.OU.alpha$B$opt$sigsq),
+                    alpha_OU=c(BM.OU.beta.log10$B$opt$alpha, NA, GBM.OU.beta.log10$B$opt$alpha, BM.OU.alpha$B$opt$alpha, NA,  GBM.OU.alpha$B$opt$alpha),
+                    sigsq_GBM=c(NA, BM.GBM.beta.log10$B$opt$sigsq, GBM.OU.beta.log10$A$opt$sigsq, NA, BM.GBM.alpha$B$opt$sigsq, GBM.OU.alpha$A$opt$sigsq),
+                    slope_GBM=c(NA, BM.GBM.beta.log10$B$opt$slope, GBM.OU.beta.log10$A$opt$slope, NA, BM.GBM.alpha$B$opt$slope, GBM.OU.alpha$A$opt$slope)
+                    )
+
 df.summary <- as.data.frame(df.summary)
 df.summary$llr <- as.numeric(as.character(df.summary$llr))
+df.summary$p_value <- as.numeric(as.character(df.summary$p_value))
+
 write.csv(df.summary, file = "data/pmc/pmc_summary.csv")
 
 
 
+# write code for table here
+#######
 
-# get p-value for log-likelihood 
-
-BM_OU.mttf <- ggplot(mttf.df[mttf.df$test == "BM_OU", c("llr", "model")], aes(x=llr, fill=model)) + 
-  geom_density(alpha=0.7) +
-  ylab("Density") +
-  xlab(TeX("Log-likelihood difference, $\\delta$")) +
-  theme_bw() +
-  geom_vline(xintercept= df.summary[df.summary$parameter == "mttf" & df.summary$test == "BM_OU" ,]$llr, linetype="dashed", color = "black") +
-  theme(plot.title = element_text(hjust = 0.5)) +
-  theme(axis.title.x = element_text(color="black", size=14), 
-        axis.title.y = element_text(color="black", size=14), 
-        panel.grid.major = element_blank(), 
-        panel.grid.minor = element_blank()) +
-  ggtitle(TeX("\\bar{T}_{d},  log_{10}")) +
-  scale_fill_brewer(palette="Set1", name="Model", 
-                    labels=c("Brownian motion", "Ornstein–Uhlenbeck"))
-  
-
-
-BM_GBM.mttf <- ggplot(mttf.df[mttf.df$test == "BM_GBM", c("llr", "model")], aes(x=llr, fill=model)) + 
-  geom_density(alpha=0.7) +
-  ylab("Density") +
-  xlab(TeX("Log-likelihood difference, $\\delta$")) +
-  theme_bw() +
-  geom_vline(xintercept= df.summary[df.summary$parameter == "mttf" & df.summary$test == "BM_GBM" ,]$llr, linetype="dashed", color = "black") +
-  theme(plot.title = element_text(hjust = 0.5)) +
-  theme(axis.title.x = element_text(color="black", size=14), 
-        axis.title.y = element_text(color="black", size=14), 
-        panel.grid.major = element_blank(), 
-        panel.grid.minor = element_blank()) +
-  ggtitle(TeX("\\bar{T}_{d},  log_{10}")) +
-  scale_fill_brewer(palette="Set1", name="Model", 
-                    labels=c("Brownian motion", "Geometric Brownian motion"))
-
-
-ggsave(file="figs/test_pmc1.png", BM_OU.mttf, units='in', dpi=600)
-ggsave(file="figs/test_pmc2.png", BM_GBM.mttf, units='in', dpi=600)
-
-
-g <- ggarrange(BM_OU.mttf, BM_GBM.mttf,                                              # First row with scatter plot
-               nrow = 1, ncol =2,
-               labels = "auto") 
-
-
-ggsave(file="figs/test_pmc.png", g, units='in', dpi=600)
 
 
 
