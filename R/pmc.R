4431106bc4e06a25861618bfbb2d64ce5e85b55f
Will Shoemaker <shoemakah@gmail.com>
2019-06-15 15:54:06 -0400

    pmc code and data update

--- R/pmc.R
+++ R/pmc.R
@@ -37,49 +37,47 @@ beta.log10 <- df$beta.log10
 names(beta.log10) <- df$Species
 alpha <- df$alpha
 names(alpha) <- df$Species
+mttf.log10 <- df$mttf.log10
+names(mttf.log10) <- df$Species
 
 
 iter <- 1000
 BM.OU.beta.log10 <- pmc(ml.rooted.um, beta.log10, "BM", "OU", nboot = iter)
 BM.OU.alpha <- pmc(ml.rooted.um, alpha, "BM", "OU", nboot = iter)
+BM.OU.mttf.log10 <- pmc(ml.rooted.um, mttf.log10, "BM", "OU", nboot = iter)
+
 BM.GBM.beta.log10 <- pmc(ml.rooted.um, beta.log10, "BM", "trend", nboot = iter)
 BM.GBM.alpha <- pmc(ml.rooted.um, alpha, "BM", "trend", nboot = iter)
+BM.GBM.mttf.log10 <- pmc(ml.rooted.um, mttf.log10, "BM", "trend", nboot = iter)
+
 # run after this
 GBM.OU.beta.log10 <- pmc(ml.rooted.um, beta.log10, "trend", "OU", nboot = iter)
 GBM.OU.alpha <- pmc(ml.rooted.um, alpha, "trend", "OU", nboot = iter)
+GBM.OU.mttf.log10 <- pmc(ml.rooted.um, mttf.log10, "trend", "OU", nboot = iter)
+
+p_value.BM.OU.beta.log10 <- length(BM.OU.beta.log10$null[BM.OU.beta.log10$null > BM.OU.beta.log10$lr] ) / iter
+p_value.BM.GBM.beta.log10 <- length(BM.GBM.beta.log10$null[BM.GBM.beta.log10$null > BM.GBM.beta.log10$lr] ) / iter
+p_value.GBM.OU.beta.log10 <- length(GBM.OU.beta.log10$null[GBM.OU.beta.log10$null > GBM.OU.beta.log10$lr] ) / iter
+
+p_value.BM.OU.alpha <- length(BM.OU.alpha$null[BM.OU.alpha$null > BM.OU.alpha$lr] ) / iter
+p_value.BM.GBM.alpha <- length(BM.GBM.alpha$null[BM.GBM.alpha$null > BM.GBM.alpha$lr] ) / iter
+p_value.GBM.OU.alpha <- length(GBM.OU.alpha$null[GBM.OU.alpha$null > GBM.OU.alpha$lr] ) / iter
+
+p_value.BM.OU.mttf.log10 <- length(BM.OU.mttf.log10$null[BM.OU.mttf.log10$null > BM.OU.mttf.log10$lr] ) / iter
+p_value.BM.GBM.mttf.log10 <- length(BM.GBM.mttf.log10$null[BM.GBM.mttf.log10$null > BM.GBM.mttf.log10$lr] ) / iter
+p_value.GBM.OU.mttf.log10 <- length(GBM.OU.mttf.log10$null[GBM.OU.mttf.log10$null > GBM.OU.mttf.log10$lr] ) / iter
 
 
-#beta.log10.ll <- do.call(c, list(BM.OU.beta.log10$null, BM.OU.beta.log10$test, BM.GBM.beta.log10$null, BM.GBM.beta.log10$test, GBM.OU.beta.log10$null, GBM.OU.beta.log10$test))
-#beta.log10.test <- do.call(c, list(replicate(iter*2, "BM_OU"), replicate(iter*2, "BM_GBM"), replicate(iter*2, "GBM_OU")))
-#beta.log10.model <- do.call(c, list(replicate(iter, "BM"), replicate(iter, "OU"), replicate(iter, "BM"), replicate(iter, "GBM"), replicate(iter, "OU"), replicate(iter, "GBM")))
-#beta.log10.df <- data.frame(llr = mttf.ll, test= mttf.test, model = mttf.model)
 
-#alpha.ll <- do.call(c, list(BM.OU.alpha$null, BM.OU.alpha$test, BM.GBM.alpha$null, BM.GBM.alpha$test, GBM.OU.alpha$null, GBM.OU.alpha$test))
-#alpha.test <- do.call(c, list(replicate(iter*2, "BM_OU"), replicate(iter*2, "BM_GBM"), replicate(iter*2, "OU_GBM")))
-#alpha.model <- do.call(c, list(replicate(iter, "BM"), replicate(iter, "OU"), replicate(iter, "BM"), replicate(iter, "GBM"), replicate(iter, "OU"), replicate(iter, "GBM")))
-#alpha.df <- data.frame(llr = alpha.ll, test= alpha.test, model = alpha.model)
-
-#write.csv(mttf.df, file = "data/pmc/pmc_beta_log10.csv")
-#write.csv(alpha.df, file = "data/pmc/pmc_alpha.csv")
-
-p_value.BM.OU.beta.log10 <- length(BM.OU.beta.log10$null[BM.OU.beta.log10$null < BM.OU.beta.log10$lr] ) / iter
-p_value.BM.GBM.beta.log10 <- length(BM.GBM.beta.log10$null[BM.GBM.beta.log10$null < BM.GBM.beta.log10$lr] ) / iter
-p_value.GBM.OU.beta.log10 <- length(GBM.OU.beta.log10$null[GBM.OU.beta.log10$null < GBM.OU.beta.log10$lr] ) / iter
-
-p_value.BM.OU.alpha <- length(BM.OU.alpha$null[BM.OU.alpha$null < BM.OU.alpha$lr] ) / iter
-p_value.BM.GBM.alpha <- length(BM.GBM.alpha$null[BM.GBM.alpha$null < BM.GBM.alpha$lr] ) / iter
-p_value.GBM.OU.alpha <- length(GBM.OU.alpha$null[GBM.OU.alpha$null < GBM.OU.alpha$lr] ) / iter
-
-
-df.summary <- cbind(parameter=c("beta.log10","beta.log10","beta.log10","alpha","alpha","alpha"), 
-                    test=c("BM_OU","BM_GBM", "GBM_OU", "BM_OU","BM_GBM", "GBM_OU"), 
-                    llr=c(BM.OU.beta.log10$lr, BM.GBM.beta.log10$lr, GBM.OU.beta.log10$lr, BM.OU.alpha$lr, BM.GBM.alpha$lr, GBM.OU.alpha$lr),
-                    p_value=c(p_value.BM.OU.beta.log10, p_value.BM.GBM.beta.log10, p_value.GBM.OU.beta.log10, p_value.BM.OU.alpha, p_value.BM.GBM.alpha, p_value.GBM.OU.alpha),
-                    sigsq_BM=c(BM.OU.beta.log10$A$opt$sigsq, BM.GBM.beta.log10$A$opt$sigsq, NA, BM.OU.alpha$A$opt$sigsq, BM.GBM.alpha$A$opt$sigsq, NA),
-                    sigsq_OU=c(BM.OU.beta.log10$B$opt$sigsq, NA, GBM.OU.beta.log10$B$opt$sigsq, BM.OU.alpha$B$opt$sigsq, NA,  GBM.OU.alpha$B$opt$sigsq),
-                    alpha_OU=c(BM.OU.beta.log10$B$opt$alpha, NA, GBM.OU.beta.log10$B$opt$alpha, BM.OU.alpha$B$opt$alpha, NA,  GBM.OU.alpha$B$opt$alpha),
-                    sigsq_GBM=c(NA, BM.GBM.beta.log10$B$opt$sigsq, GBM.OU.beta.log10$A$opt$sigsq, NA, BM.GBM.alpha$B$opt$sigsq, GBM.OU.alpha$A$opt$sigsq),
-                    slope_GBM=c(NA, BM.GBM.beta.log10$B$opt$slope, GBM.OU.beta.log10$A$opt$slope, NA, BM.GBM.alpha$B$opt$slope, GBM.OU.alpha$A$opt$slope)
+df.summary <- cbind(parameter=c("beta.log10","beta.log10","beta.log10","alpha","alpha","alpha", "mttf.log10", "mttf.log10", "mttf.log10"), 
+                    test=c("BM_OU","BM_GBM", "GBM_OU", "BM_OU","BM_GBM", "GBM_OU", "BM_OU","BM_GBM", "GBM_OU"), 
+                    llr=c(BM.OU.beta.log10$lr, BM.GBM.beta.log10$lr, GBM.OU.beta.log10$lr, BM.OU.alpha$lr, BM.GBM.alpha$lr, GBM.OU.alpha$lr, BM.OU.mttf.log10$lr, BM.GBM.mttf.log10$lr, GBM.OU.mttf.log10$lr),
+                    p_value=c(p_value.BM.OU.beta.log10, p_value.BM.GBM.beta.log10, p_value.GBM.OU.beta.log10, p_value.BM.OU.alpha, p_value.BM.GBM.alpha, p_value.GBM.OU.alpha, p_value.BM.OU.mttf.log10, p_value.BM.GBM.mttf.log10, p_value.GBM.OU.mttf.log10),
+                    sigsq_BM=c(BM.OU.beta.log10$A$opt$sigsq, BM.GBM.beta.log10$A$opt$sigsq, NA, BM.OU.alpha$A$opt$sigsq, BM.GBM.alpha$A$opt$sigsq, NA, BM.OU.mttf.log10$A$opt$sigsq, BM.GBM.mttf.log10$A$opt$sigsq, NA),
+                    sigsq_OU=c(BM.OU.beta.log10$B$opt$sigsq, NA, GBM.OU.beta.log10$B$opt$sigsq, BM.OU.alpha$B$opt$sigsq, NA,  GBM.OU.alpha$B$opt$sigsq, BM.OU.mttf.log10$B$opt$sigsq, NA,  GBM.OU.mttf.log10$B$opt$sigsq),
+                    alpha_OU=c(BM.OU.beta.log10$B$opt$alpha, NA, GBM.OU.beta.log10$B$opt$alpha, BM.OU.alpha$B$opt$alpha, NA,  GBM.OU.alpha$B$opt$alpha, BM.OU.mttf.log10$B$opt$alpha, NA,  GBM.OU.mttf.log10$B$opt$alpha),
+                    sigsq_GBM=c(NA, BM.GBM.beta.log10$B$opt$sigsq, GBM.OU.beta.log10$A$opt$sigsq, NA, BM.GBM.alpha$B$opt$sigsq, GBM.OU.alpha$A$opt$sigsq, NA, BM.GBM.mttf.log10$B$opt$sigsq, GBM.OU.mttf.log10$A$opt$sigsq),
+                    slope_GBM=c(NA, BM.GBM.beta.log10$B$opt$slope, GBM.OU.beta.log10$A$opt$slope, NA, BM.GBM.alpha$B$opt$slope, GBM.OU.alpha$A$opt$slope, NA, BM.GBM.mttf.log10$B$opt$slope, GBM.OU.mttf.log10$A$opt$slope)
                     )
 
 df.summary <- as.data.frame(df.summary)
